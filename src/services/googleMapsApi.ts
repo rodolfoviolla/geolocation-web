7107c2d31adbe6e6c3f63b605cd89b83fd29b672
Rodolfo Della Violla <rodolfo.violla@gmail.com>
2022-03-12 14:26:34 -0300

    feature: finishes first version

---
+++ src/services/googleMapsApi.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { GOOGLE_MAPS_API_URL } from '../constants';
+
+export const googleMapsApi = axios.create({
+  baseURL: GOOGLE_MAPS_API_URL
+});
---
+++ src/services/openWeatherApi.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { OPEN_WEATHER_API_URL } from '../constants';
+
+export const openWeatherApi = axios.create({
+  baseURL: OPEN_WEATHER_API_URL
+});
